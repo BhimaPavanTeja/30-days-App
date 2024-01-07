38b8ac7713b9fdb03b8b1493e380ad418efdd532
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2024-01-07 13:26:49 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/a30daysapp/MainActivity.kt
+++ app/src/main/java/com/example/a30daysapp/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -35,13 +34,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.a30daysapp.data.Fit
 import com.example.a30daysapp.data.images
 import com.example.a30daysapp.ui.theme._30DaysAppTheme
@@ -70,10 +67,18 @@ fun GymApp() {
     Scaffold(
         topBar = {
             GymTopAppBar()
-        }
+        } // Best practice is to use compressed images in LazyColumn
+        // in order to get smooth scrolling.
     ) { it ->
-        LazyColumn(contentPadding = it) {
-            items(images) { // Assuming gymList is your list of Gym objects
+        LazyColumn(
+            contentPadding = it
+        ) {
+            items(
+                items = images,
+                key = {
+                    it.id // Unique identifier for every item.
+                }
+            ) { // Assuming gymList is your list of Gym objects.
                 GymItem(
                     gym = it,
                     modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
